33a3c5b5587045468fd915cff971bf0bc1883f8d
GodReorx <155961655+GodReorx@users.noreply.github.com>
2024-01-24 17:37:45 +0100

    Add files via upload

---
+++ Sprint_1/S1.07.Annotations/n1exercici1/ClassWorker/Worker.java
@@ -0,0 +1,41 @@
+package ClassWorker;
+
+public class Worker {
+    String name;
+    String surname;
+    int priceHour;
+
+    public Worker(String name, String surname, int priceHour) {
+        this.name = name;
+        this.surname = surname;
+        this.priceHour = priceHour;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getPriceHour() {
+        return priceHour;
+    }
+
+    public void setPriceHour(int priceHour) {
+        this.priceHour = priceHour;
+    }
+
+    public int calculateSalary(int hours){
+        return hours * priceHour;
+    }
+}
---
+++ Sprint_1/S1.07.Annotations/n1exercici1/ClassWorker/WorkerOnline.java
@@ -0,0 +1,16 @@
+package ClassWorker;
+
+public class WorkerOnline extends Worker {
+
+    static final int INTERNET = 60;
+
+    public WorkerOnline(String name, String surname, int priceHour) {
+        super(name, surname, priceHour);
+    }
+
+    @Override
+    public int calculateSalary(int hours){
+        return (hours * super.priceHour) + INTERNET;
+    }
+
+}
