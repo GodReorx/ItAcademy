6d429d9418d855c47eda470c21991b4267c98abc
GodReorx <155961655+GodReorx@users.noreply.github.com>
2024-01-29 12:30:57 +0100

    Add files via upload
    Modificación para el funcionamiento correcto del reverse

--- Sprint_1/S1.08.Lambdas/n1exercici8/InitTools/Starter.java
+++ Sprint_1/S1.08.Lambdas/n1exercici8/InitTools/Starter.java
@@ -5,10 +5,10 @@ import SortListMethod.*;
 public class Starter {
     public static void startApp(){
 
-        Reverser reverser = (str) -> new StringBuilder().reverse().toString();
+        Reverser reverserStr = str -> new StringBuilder(str).reverse().toString();
 
         String str = "Hola";
-        String reverseStr = reverser.reverseMethod(str);
-        System.out.println("Reversed : " + reverseStr.length());
+        String reverseStr = reverserStr.reverseMethod(str);
+        System.out.println("Reversed : " + reverseStr);
     }
 }
